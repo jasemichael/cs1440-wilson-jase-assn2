2c19704eb872f5a50d5d6d87757154aa851e9ffe
Jase M. Wilson <jase.wilson@aggiemail.usu.edu>
2020-02-21 23:57:14 -0700

    Changed output

--- src/main.py
+++ src/main.py
@@ -45,15 +45,15 @@ for line in f:
 			all_num_areas += 1
 			all_gross_annual_wages += int(l[10].strip('"'))
 			if all_max_annual_wage[1] < int(l[10].strip('"')):
-				all_max_annual_wage[0] = d[l[0]]
+				all_max_annual_wage[0] = d[l[0]].strip('"')
 				all_max_annual_wage[1] = int(l[10].strip('"'))
 			all_total_estab += int(l[8].strip('"'))
 			if all_max_estab[1] < int(l[8].strip('"')):
-				all_max_estab[0] = d[l[0]]
+				all_max_estab[0] = d[l[0]].strip('"')
 				all_max_estab[1] = int(l[8].strip('"'))
 			all_total_empl += int(l[9].strip('"'))
 			if all_max_empl[1] < int(l[9].strip('"')):
-				all_max_empl[0] = d[l[0]]
+				all_max_empl[0] = d[l[0]].strip('"')
 				all_max_empl[1] = int(l[9].strip('"'))
 
 	if l[0].strip('"')[0] != 'C' and l[0].strip('"')[0] != 'U' and '000' != l[0].strip('"')[2:5] and len(l[0].strip('"')) == 5:
@@ -61,15 +61,15 @@ for line in f:
 			soft_num_areas += 1
 			soft_gross_annual_wages += int(l[10].strip('"'))
 			if soft_max_annual_wage[1] < int(l[10].strip('"')):
-				soft_max_annual_wage[0] = d[l[0]]
+				soft_max_annual_wage[0] = d[l[0]].strip('"')
 				soft_max_annual_wage[1] = int(l[10].strip('"'))
 			soft_total_estab += int(l[8].strip('"'))
 			if soft_max_estab[1] < int(l[8].strip('"')):
-				soft_max_estab[0] = d[l[0]]
+				soft_max_estab[0] = d[l[0]].strip('"')
 				soft_max_estab[1] = int(l[8].strip('"'))
 			soft_total_empl += int(l[9].strip('"'))
 			if soft_max_empl[1] < int(l[9].strip('"')):
-				soft_max_empl[0] = d[l[0]]
+				soft_max_empl[0] = d[l[0]].strip('"')
 				soft_max_empl[1] = int(l[9].strip('"'))
 
 after = time.time()
